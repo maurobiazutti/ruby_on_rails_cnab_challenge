667aa60db06cdf8f94578bd73c15a29c057e06ee
mauro <mauro_biazutti@hotmail.com>
2025-06-04 23:18:40 -0300

    feat: rendering form upload on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index ]
   def index
+    @uploaded_file = UploadedFile.new
   end
 end
--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -11,7 +11,7 @@ class UploadsController < ApplicationController
     if @uploaded_file.save
       redirect_to root_path, notice: "Arquivo enviado com sucesso."
     else
-      render :new
+      render 'home/index', status: :unprocessable_entity
     end
   end
 
