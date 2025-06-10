f48d8e28a06b88cbc039b227666112426eac6dba
mauro <mauro_biazutti@hotmail.com>
2025-06-10 19:49:32 -0300

    feat(insert/seed): inserting data into TransactionType tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,7 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
+# Create default transaction types
 TransactionType.create!([
   { code: 1, description: "Débito", nature: "Entrada", signal: "+" },
   { code: 2, description: "Boleto", nature: "Saída", signal: "-" },
